eab32d80774fce3bf2a79ffc32a36d0f56b64d26
Uttamkr1213 <165399041+Uttamkr1213@users.noreply.github.com>
2024-04-03 20:51:35 +0000

    added requirements.txt also commits were not properly done so here is all the updates

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+topics_dict= [
+  {
+  "title":"Ladakh",
+  "description":"Fast by Funsuk",
+  "posted_by":"Rajesh"
+  },
+  {
+  "title":"Forest",
+  "description":"Forest cutting in MP"
+  }
+]
 
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',topics=topics_dict)
+@app.route('/api/topics')
+def list_topics():
+  return jsonify(topics_dict)
+  
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug = True)
